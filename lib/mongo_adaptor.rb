d7403692e49a0aba02caab89300805e7d13d2eee
Jon Rowe <hello@jonrowe.co.uk>
2012-10-02 13:34:31 +0100

    inserting structs

--- lib/mongo_adaptor.rb
+++ lib/mongo_adaptor.rb
@@ -8,4 +8,24 @@ class MongoAdaptor
       @db ||= Mongo::Configure.current.load
     end
   end
+
+  def initialize(name,klass)
+    @collection = self.class.db.collection name.to_s.downcase
+    @klass = klass
+  end
+
+  def primary_key
+    "_id"
+  end
+
+  def insert(model)
+    @collection.insert( process(model), { safe: true } )
+  end
+
+  private
+    def process(model)
+      fields = {}
+      model.each_pair { |field,value| fields[field] = value unless field == primary_key.to_sym }
+      fields
+    end
 end
--- spec/mongo_adaptor_spec.rb
+++ spec/mongo_adaptor_spec.rb
@@ -1,14 +1,39 @@
 require 'mongo_adaptor'
 
-describe 'db setup' do
-  let(:config) { Mongo::Configure.from_database 'mongo_adaptor_test' }
-
+describe 'adapting structs into mongo' do
   before do
-    config
+    Mongo::Configure.from_database 'mongo_adaptor_test'
   end
 
-  it 'uses the configured database' do
-    MongoAdaptor.db.name.should == 'mongo_adaptor_test'
+  describe 'db setup' do
+    it 'uses the configured database' do
+      MongoAdaptor.db.name.should == 'mongo_adaptor_test'
+    end
   end
 
+  describe 'inserting a blank model' do
+    let(:adaptor)    { MongoAdaptor.new 'test_collection', nil }
+    let(:model)      { Struct.new(:name,:other,:_id).new('Test Model','Some Data','fake key') }
+    let(:collection) { Mongo::Configure.current.load.collection 'test_collection' }
+    let(:data)       { collection.find({}).to_a[-1] }
+
+    after do
+      Mongo::Configure.current.load.collections.select { |c| c.name !~ /^system\./ }.each &:remove
+    end
+
+    subject { adaptor.insert model }
+
+    it 'changes the number of items in the collection' do
+      expect { subject }.to change { collection.size }.by(1)
+    end
+    it 'generates an _id, ignoring any set key' do
+      subject
+      data['_id'].should be_a BSON::ObjectId
+    end
+    it 'sets my fields and values' do
+      subject
+      data['name'].should  == 'Test Model'
+      data['other'].should == 'Some Data'
+    end
+  end
 end
