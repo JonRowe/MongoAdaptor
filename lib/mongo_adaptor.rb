e7a6caf743fba517fb61a2ac8765e5bd9bdca3e7
Jon Rowe <hello@jonrowe.co.uk>
2012-10-03 11:12:36 +0100

    use member test rather than methods

--- lib/mongo_adaptor.rb
+++ lib/mongo_adaptor.rb
@@ -35,7 +35,7 @@ class MongoAdaptor
         @klass.new.tap do |model|
           model[:id] = result.delete('_id') if model.respond_to?(:id)
           result.each do |field,value|
-            model[field] = value if model.respond_to? field
+            model[field] = value if model.members.include? field.to_sym
           end
         end
       end
